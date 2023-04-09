0673b0c2cee924565306fa079911134846538ce4
Moritz Freyburger <moritz@freyburger.io>
2023-04-09 22:10:50 +0200

    Refactor

--- src/input.rs
+++ src/input.rs
@@ -14,68 +14,76 @@ pub enum Input {
     Number(u32),
 }
 
+enum Loop {
+    Continue,
+    Break,
+}
+
 pub struct InputLoop {
     sender: Sender<Input>,
-    should_stop: bool,
+}
+
+impl TryFrom<Event> for Input {
+    type Error = ();
+
+    fn try_from(value: Event) -> Result<Self, Self::Error> {
+        match value {
+            Event::Key(e) => match e.code {
+                KeyCode::Up => Ok(Input::Rotate),
+                KeyCode::Right => Ok(Input::Right),
+                KeyCode::Left => Ok(Input::Left),
+                KeyCode::Down => Ok(Input::Down),
+                KeyCode::Char('d') => Ok(Input::Drop),
+                KeyCode::Char(' ') => Ok(Input::Drop),
+                KeyCode::Char('q') => Ok(Input::Quit),
+                KeyCode::Char('r') => Ok(Input::Restart),
+                KeyCode::Char('c') => match e.modifiers {
+                    KeyModifiers::CONTROL => Ok(Input::Quit),
+                    _ => Err(()),
+                },
+                KeyCode::Char(a) => match a {
+                    '0'..='9' => Ok(Input::Number(a.to_digit(10).expect("Should not fail"))),
+                    _ => Err(()),
+                },
+                _ => Err(()),
+            },
+            _ => Err(()),
+        }
+    }
 }
 
 impl InputLoop {
     pub fn new(sender: Sender<Input>) -> Self {
-        Self {
-            sender,
-            should_stop: false,
+        Self { sender }
+    }
+
+    pub fn run(&self) {
+        loop {
+            match self.provide_inputs() {
+                Loop::Continue => continue,
+                Loop::Break => break,
+            };
         }
     }
 
-    pub fn run(&mut self) {
-        while !self.should_stop {
-            self.provide_inputs();
-        }
-    }
-
-    fn provide_inputs(&mut self) {
+    fn provide_inputs(&self) -> Loop {
         let input = match crossterm::event::read() {
-            Ok(e) => self.read_input(&e),
+            Ok(e) => Input::try_from(e).ok(),
             Err(_) => None,
         };
 
         if let Some(i) = input {
-            self.stop_if_necessary(&i);
             self.sender.send(i).ok();
+            return self.determine_result(&i);
         }
+
+        Loop::Continue
     }
 
-    fn read_input(&self, event: &Event) -> Option<Input> {
-        match event {
-            Event::Key(e) => match e.code {
-                KeyCode::Up => Some(Input::Rotate),
-                KeyCode::Right => Some(Input::Right),
-                KeyCode::Left => Some(Input::Left),
-                KeyCode::Down => Some(Input::Down),
-                KeyCode::Char('d') => Some(Input::Drop),
-                KeyCode::Char(' ') => Some(Input::Drop),
-                KeyCode::Char('q') => Some(Input::Quit),
-                KeyCode::Char('r') => Some(Input::Restart),
-                KeyCode::Char('c') => match e.modifiers {
-                    KeyModifiers::CONTROL => Some(Input::Quit),
-                    _ => None,
-                },
-                KeyCode::Char(a) => match a {
-                    '0'..='9' => Some(Input::Number(a.to_digit(10).expect("Should not fail"))),
-                    _ => None,
-                },
-                _ => None,
-            },
-            _ => None,
-        }
-    }
-
-    fn stop_if_necessary(&mut self, input: &Input) {
+    fn determine_result(&self, input: &Input) -> Loop {
         match input {
-            Input::Quit | Input::Restart => {
-                self.should_stop = true;
-            }
-            _ => {}
-        };
+            Input::Quit | Input::Restart => Loop::Break,
+            _ => Loop::Continue,
+        }
     }
 }
